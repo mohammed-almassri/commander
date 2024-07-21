9e495cfea9bb807a07f8d216df0b2e31379a67ad
mohammed-almassri <mohammed.m.almassri@gmail.com>
2024-07-21 18:04:27 +0300

    main functionality

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,157 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  const commanderProvider = new CommanderProvider();
+  vscode.window.registerTreeDataProvider("commanderView", commanderProvider);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "commander" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('commander.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from commander!');
-	});
-
-	context.subscriptions.push(disposable);
+  vscode.commands.registerCommand("commander.refresh", () =>
+    commanderProvider.refresh()
+  );
+  vscode.commands.registerCommand(
+    "commander.runCommand",
+    (node: CommandNode) => {
+      if (node.split) {
+        runCommandsInSplitTerminals(node.command.arguments![1] as string[]);
+      } else {
+        const terminal = vscode.window.createTerminal("Commander");
+        terminal.show();
+        if (Array.isArray(node.command)) {
+          for (const cmd of node.command) {
+            terminal.sendText(cmd);
+          }
+        } else {
+          if (validateCommand(node.command.arguments![1])) {
+            terminal.sendText(node.command.arguments![1] as string);
+          } else {
+            vscode.window.showErrorMessage(
+              "Command not allowed for security reasons."
+            );
+          }
+        }
+      }
+    }
+  );
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration("commander.commands")) {
+      commanderProvider.refresh();
+    }
+  });
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function runCommandsInSplitTerminals(commands: string | string[]) {
+  if (typeof commands === "string") {
+    commands = [commands];
+  }
+
+  let parentTerminal: vscode.Terminal | undefined;
+
+  for (const command of commands) {
+    const terminalOptions: vscode.TerminalOptions = {
+      name: `Commander: ${command}`,
+    };
+
+    if (parentTerminal) {
+      terminalOptions.location = { parentTerminal: parentTerminal };
+    }
+
+    const terminal = vscode.window.createTerminal(terminalOptions);
+    terminal.show();
+    if (validateCommand(command)) {
+      terminal.sendText(command);
+    } else {
+      vscode.window.showErrorMessage(
+        "Command not allowed for security reasons."
+      );
+    }
+
+    if (!parentTerminal) {
+      parentTerminal = terminal;
+    }
+  }
+}
+
+function validateCommand(command: string): boolean {
+  const restrictedPatterns = [
+    /rm\s+-rf\s+\//,
+    /shutdown\s+/,
+    /reboot\s+/,
+    /dd\s+if=\/dev\/.*/,
+    /mkfs\s+/,
+    /mkswap\s+/,
+    /init\s+/,
+    /poweroff\s+/,
+    /halt\s+/,
+    /swapoff\s+/,
+    /swapon\s+/,
+    /umount\s+/,
+    /fsck\s+/,
+  ];
+
+  return !restrictedPatterns.some((pattern) => pattern.test(command));
+}
+
+
+class CommanderProvider implements vscode.TreeDataProvider<CommandNode> {
+  private _onDidChangeTreeData: vscode.EventEmitter<CommandNode | undefined> =
+    new vscode.EventEmitter<CommandNode | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<CommandNode | undefined> =
+    this._onDidChangeTreeData.event;
+
+  private commands: CommandNode[];
+
+  constructor() {
+    this.commands = this.getCommandsFromConfig();
+  }
+
+  private getCommandsFromConfig(): CommandNode[] {
+    const config = vscode.workspace.getConfiguration("commander");
+    const userCommands = config.get<
+      { label: string; command: string; split: boolean }[]
+    >("commands", []);
+    return userCommands.map(
+      (cmd) =>
+        new CommandNode(
+          cmd.label,
+          {
+            title: cmd.label,
+            command: cmd.command,
+          },
+          cmd.split
+        )
+    );
+  }
+
+  refresh(): void {
+    this.commands = this.getCommandsFromConfig();
+    this._onDidChangeTreeData.fire(undefined);
+  }
+
+  getTreeItem(element: CommandNode): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: CommandNode): Thenable<CommandNode[]> {
+    if (element) {
+      return Promise.resolve([]);
+    } else {
+      return Promise.resolve(this.commands);
+    }
+  }
+}
+
+class CommandNode extends vscode.TreeItem {
+  constructor(
+    public readonly label: string,
+    public readonly command: vscode.Command,
+    public readonly split: boolean
+  ) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+    this.tooltip = `${this.label}`;
+    this.command = {
+      command: "commander.runCommand",
+      title: "",
+      arguments: [this, command.command],
+    };
+    this.iconPath = new vscode.ThemeIcon("terminal");
+  }
+}
